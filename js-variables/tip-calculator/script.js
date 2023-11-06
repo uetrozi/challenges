00f657cf8590cf63ee04985b41f718a25838a6d7
uetrozi <charlotte_keuer@posteo.de>
2023-11-06 10:42:52 +0100

    Init commit

---
+++ js-variables/tip-calculator/script.js
@@ -0,0 +1,8 @@
+let mealCost = 30;
+let tipPercentage = 0.1;
+
+let tipAmount = mealCost * tipPercentage;
+
+let totalCost = mealCost + tipAmount;
+
+console.log("// The total cost of your bill is:", totalCost, "euros");
