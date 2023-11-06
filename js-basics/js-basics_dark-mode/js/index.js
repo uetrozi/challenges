4bfaff119b9894b06de740cf429f2df7c136da96
uetrozi <charlotte_keuer@posteo.de>
2023-11-06 11:40:49 +0100

    activate buttons in index js

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkBtn = document.querySelector('[data-js="dark-mode-button"]');
+const lightBtn = document.querySelector('[data-js="light-mode-button"]');
+const toggleBtn = document.querySelector('[data-js="toggle-button"]');
+
+// console.log(darkBtn);
+
+darkBtn.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+lightBtn.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+toggleBtn.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
