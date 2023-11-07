1d3d91e920682d958664038b322372f9c629b3d2
uetrozi <charlotte_keuer@posteo.de>
2023-11-07 11:15:43 +0100

    finish array with test

--- js-objects/js-objects-and-arrays_arrays/index.js
+++ js-objects/js-objects-and-arrays_arrays/index.js
@@ -22,39 +22,37 @@ const firstNumber = numbers[0];
 
 const fruits = ["apple", "banana", "grapefruit"];
 fruits[0] = "mango";
-console.log(fruits[0]);
 
 // EXERCISE 5
 // Change the value of nestedNumber to equal the fourth number of nestedNumbers using bracket notation.
 
 const nestedNumbers = [10, [20, 30, [40, 50]]];
 
-const nestedNumber = nestedNumbers[4];
-console.log(nestedNumber);
+const nestedNumber = nestedNumbers[1][2][0];
 
 // EXERCISE 6
 // Put "rat" onto the end of petsWithPush variable by using the array method `.push()`.
 
 const petsWithPush = ["dog", "cat", "rabbit"];
-console.log(petsWithPush.push("rat"));
+petsWithPush.push("rat");
 
 // EXERCISE 7
 // Use the pop method to remove the last item from fruitsWithPop.
 
 const fruitsWithPop = ["apple", "banana", "mango"];
-console.log(fruitsWithPop.pop());
+fruitsWithPop.pop();
 
 // EXERCISE 8
 // Put "hamster" onto the start of unshiftedPets variable by using the array method unshift.
 
 const unshiftedPets = ["dog", "cat", "rabbit"];
-console.log(unshiftedPets.unshift("hamster"));
+unshiftedPets.unshift("hamster");
 
 // EXERCISE 9
 // Use the shift method to remove the first item from shiftedFruits.
 
 const shiftedFruits = ["apple", "banana", "mango"];
-console.log(shiftedFruits.shift());
+shiftedFruits.shift();
 
 export {
   exampleArray,
