dc1b78f57e1fcd992410081cea58d80553b17da7
uetrozi <charlotte_keuer@posteo.de>
2023-11-09 21:19:08 +0100

    add event listener to inputs

--- js-inputs-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -16,3 +16,19 @@ For further information check MDN:
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
 */
+
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
+
+inputColor.addEventListener("click", () => {
+  console.log(inputColor.value);
+});
+
+inputRadius.addEventListener("click", () => {
+    console.log(inputRadius.value);
+  });
+
+inputRotation.addEventListener("click", () => {
+  console.log(inputRotation.value);
+});
