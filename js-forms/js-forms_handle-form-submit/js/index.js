a02d29ef21f38a03cc6aebbca92ae471af154b82
uetrozi <charlotte_keuer@posteo.de>
2023-11-10 12:15:51 +0100

    finish handle form submit

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+
+  const age = event.target.elements.age.value;
+  const badness = event.target.elements.badness.value;
+  const ageBadnessSum = age + badness;
+  console.log("The age-badness-sum is: " + ageBadnessSum);
+});
