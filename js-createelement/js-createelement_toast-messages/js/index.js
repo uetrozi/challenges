4f4e5bc48aa1e9811ed33109f07f51c83feebfb3
uetrozi <charlotte_keuer@posteo.de>
2023-11-11 22:09:09 +0100

    init commit

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -4,3 +4,13 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+addButton.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.classList.add("toast-container__message");
+  li.textContent = "I'm new!";
+  toastContainer.append(li);
+});
+
+clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
+});
