382bb65e3846b88fda47e7e4e793460c9ffdb6d6
uetrozi <charlotte_keuer@posteo.de>
2023-11-17 17:52:30 +0100

    finish quiz app

---
+++ js-structure/js-structure_quiz-app/components/CardList/cardList.js
@@ -0,0 +1,13 @@
+import { cards, Card } from "./../Card/cards.js";
+
+export function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
