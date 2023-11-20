0f057bac0a1086e9479cdcc8ac74bd51c53415b4
uetrozi <charlotte_keuer@posteo.de>
2023-11-20 19:16:31 +0100

    finish news flash

--- array-methods2/js-array-methods-2_news-flash/index.js
+++ array-methods2/js-array-methods-2_news-flash/index.js
@@ -5,12 +5,25 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
+const filteredNews = news.filter((properties) => {
+  return properties.categories.includes("politics");
 });
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.slice().sort((a, b) => {
+  const lengthA = a.body.length;
+  const lengthB = b.body.length;
+
+  if (lengthA < lengthB) {
+    return -1;
+  }
+  if (lengthA > lengthB) {
+    return 1;
+  }
+  return 0;
+});
+
+console.log(sortedNews);
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
