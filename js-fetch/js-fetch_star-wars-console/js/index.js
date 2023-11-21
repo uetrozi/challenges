ee221bf40755ffbe2298cb64e6c0359b1602fbc5
uetrozi <charlotte_keuer@posteo.de>
2023-11-21 13:54:58 +0100

    finish star wars console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,24 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const starWarsObjects = data.results;
+      const r2d2 = starWarsObjects.find((person) =>
+        person.name.startsWith("R2")
+      );
+
+      console.log(r2d2.eye_color);
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+}
 
 fetchData();
