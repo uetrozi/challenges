9150322a8d54f024295e5219260285c300335cd5
uetrozi <charlotte_keuer@posteo.de>
2023-11-22 13:21:37 +0100

    finish get animals

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal == "cats") {
+    return "I totally love cats!";
+  }
+  if (animal == `${animal}`) {
+    return `I like ${animal}!`;
+  }
+  if (animal == null) {
+    return "I do not like animals at all!";
+  }
+}
