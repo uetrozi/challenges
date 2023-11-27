7568dc7068dc205f77ef33a33d0beb8c064c5498
uetrozi <charlotte_keuer@posteo.de>
2023-11-27 16:14:25 +0100

    finish smilie

--- react-props/react-props_smiley/src/App.js
+++ react-props/react-props_smiley/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <CreateSmiley isHappy={false} />;
+}
+
+function CreateSmiley({ isHappy }) {
+  return <div>{isHappy ? <span>😊 </span> : <span>🥺</span>}</div>;
 }
