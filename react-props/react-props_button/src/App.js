1f5a7825f232a5e7a0d7a582f151fb9d0c29157e
uetrozi <charlotte_keuer@posteo.de>
2023-11-27 17:25:49 +0100

    finish buttons

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -2,5 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("You clicked me!");
+  }
+  return (
+    <CreateButton
+      color="green"
+      disabled={false}
+      text="What a nice button!"
+      onClick={handleClick}
+    />
+  );
+}
+
+function CreateButton({ color, disabled, text, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      style={{
+        height: "100px",
+        width: "300px",
+        borderRadius: "50px",
+        backgroundColor: color,
+        color: "White",
+      }}
+    >
+      {text}
+    </button>
+  );
 }
