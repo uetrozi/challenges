8d5c7da8770202716ed63a3b9dc9746e0b7b24fa
uetrozi <charlotte_keuer@posteo.de>
2023-11-27 15:57:10 +0100

    finish greetings

--- react-props/react-props_greeting/src/App.js
+++ react-props/react-props_greeting/src/App.js
@@ -1,5 +1,13 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Maria" />;
+}
+
+function Greeting({ name }) {
+  if (name == "Gimena" || name == "Sven") {
+    return <h1>Hello, Coach! </h1>;
+  } else {
+    return <h1>Hello, {name}! </h1>;
+  }
 }
