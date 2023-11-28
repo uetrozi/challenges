ec9a0cd49c9f81b1e395ffad70dab5efa3d92004
uetrozi <charlotte_keuer@posteo.de>
2023-11-28 10:47:54 +0100

    finish buttons and children

--- react-nesting/react-nesting_buttons-and-children/src/App.js
+++ react-nesting/react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me</Button>
+      <Button>No Click me!</Button>
+      <Button>Click me first, then the others!</Button>
+      <Button><p> I dont mind if you click me</p></Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
