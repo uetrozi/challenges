e742b8d3723a5deb5aae8d6752e4164d0aa53fdd
uetrozi <charlotte_keuer@posteo.de>
2023-11-29 10:55:17 +0100

    finish counter

--- react-states/react-state_counter/src/App.js
+++ react-states/react-state_counter/src/App.js
@@ -1,27 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="container">
-      <h1>{count}</h1>
+      <h1>{counter}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={() => setCounter(counter - 1)}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={() => setCounter(counter + 1)}>
           +
         </button>
       </div>
