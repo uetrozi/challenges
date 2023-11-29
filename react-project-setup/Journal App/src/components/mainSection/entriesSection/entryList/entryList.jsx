1a1787acd2aa6a250fe957d047fb02cbfeda9da0
uetrozi <charlotte_keuer@posteo.de>
2023-11-29 14:49:43 +0100

    arggghh

--- react-project-setup/Journal App/src/components/mainSection/entriesSection/entryList/entryList.jsx
+++ react-project-setup/Journal App/src/components/mainSection/entriesSection/entryList/entryList.jsx
@@ -1,12 +1,18 @@
 import Entry from "./entry.jsx";
 
+const EntryData = {
+  title: "This is a title",
+  date: "04.05.2022",
+  text: "Lorem pisom dolores",
+};
+
 export default function EntryList() {
   return (
     <div className="entryList">
       <Entry
-        title="This is a title"
-        date="04.05.2022"
-        text="lorem impsum dolores ips"
+        title={EntryData.title}
+        date={EntryData.date}
+        text={EntryData.text}
       />
     </div>
   );
