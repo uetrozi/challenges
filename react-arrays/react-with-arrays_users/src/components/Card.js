70ac1843e0b5b8d31ee163057c13b5c4f33b237e
uetrozi <charlotte_keuer@posteo.de>
2023-11-29 15:54:02 +0100

    finish users

--- react-arrays/react-with-arrays_users/src/components/Card.js
+++ react-arrays/react-with-arrays_users/src/components/Card.js
@@ -2,11 +2,14 @@ import "./Card.css";
 import Tag from "./Tag";
 import Button from "./Button";
 function Card({ user }) {
+  const userRoles = user.roles;
   return (
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {userRoles.map((roles) => (
+          <Tag key={roles} tag={roles} />
+        ))}
       </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
