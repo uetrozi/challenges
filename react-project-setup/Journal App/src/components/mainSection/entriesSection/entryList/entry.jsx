03b33f4b2796c346732ee94a12ae5a855cf83933
uetrozi <charlotte_keuer@posteo.de>
2023-11-29 09:37:17 +0100

    some more css for favorited button

--- react-project-setup/Journal App/src/components/mainSection/entriesSection/entryList/entry.jsx
+++ react-project-setup/Journal App/src/components/mainSection/entriesSection/entryList/entry.jsx
@@ -4,9 +4,9 @@ export default function Entry({ title, date, text, isFavorited }) {
   return (
     <li className="entry">
       <span className="entry-date">{date}</span>
+      <IconButton isFavorited={isFavorited} />
       <h3 className="entry-title">{title}</h3>
       <p className="entry-text">{text}</p>
-      <IconButton isFavorited={isFavorited} />
     </li>
   );
 }
--- react-project-setup/Journal App/src/components/mainSection/entriesSection/entryList/iconButton.jsx
+++ react-project-setup/Journal App/src/components/mainSection/entriesSection/entryList/iconButton.jsx
@@ -3,12 +3,12 @@ import starFilled from "../../../../assets/star-filled.svg";
 
 export default function IconButton({ isFavorited }) {
   return isFavorited ? (
-    <button type="button" className="favorite__iconButton">
-      <img src={starFilled} alt="star-filled"></img>
+    <button type="button" className="icon-button">
+      <img className="starButton-image" src={starFilled} alt="star-filled"></img>
     </button>
   ) : (
-    <button type="button" className="favorite__iconButton">
-      <img src={star} alt="star"></img>
+    <button type="button" className="icon-button">
+      <img className="starButton-image" src={star} alt="star"></img>
     </button>
   );
 }
