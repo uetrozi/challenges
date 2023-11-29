b1bbc3a13f17994852754374729cf4732de2c059
uetrozi <charlotte_keuer@posteo.de>
2023-11-29 15:09:09 +0100

    finish fruit

--- react-arrays/react-with-arrays_fruits/src/App.js
+++ react-arrays/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,44 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 0,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1,
+      name: "🍒 Cherry",
+      color: "red",
+    },
+    {
+      id: 2,
+      name: "🍏 Apple",
+      color: "green",
+    },
+    {
+      id: 3,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: 4,
+      name: "🫐 blueberry",
+      color: "blue",
+    },
+    {
+      id: 5,
+      name: "🍇 grapes",
+      color: "purple",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} />
+      ))}
     </div>
   );
 }
