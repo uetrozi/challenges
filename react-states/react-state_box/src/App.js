cf185af335eb60fdb77ae920205f2e8c73874c93
uetrozi <charlotte_keuer@posteo.de>
2023-11-29 10:42:54 +0100

    finish box

--- react-states/react-state_box/src/App.js
+++ react-states/react-state_box/src/App.js
@@ -1,14 +1,14 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
     // Check that the value changes correctly.
-    console.log(isActive);
   }
-
+  console.log(isActive);
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
