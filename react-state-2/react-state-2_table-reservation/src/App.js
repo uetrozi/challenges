51fe4868510c473ca03a76459de476dfc0726f4d
uetrozi <charlotte_keuer@posteo.de>
2023-11-30 17:14:39 +0100

    finish table reservation

--- react-state-2/react-state-2_table-reservation/src/App.js
+++ react-state-2/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleReservation(event) {
+    if (event.target.textContent == "+") {
+      setPeople(people + 1);
+    } else {
+      setPeople(people - 1);
+    }
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onhandleReservation={handleReservation} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
