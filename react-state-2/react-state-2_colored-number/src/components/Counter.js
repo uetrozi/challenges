fc5b778a0aa5e7d08f7fb26adc298e09c24748dc
uetrozi <charlotte_keuer@posteo.de>
2023-11-30 14:43:58 +0100

    finish colored number

--- react-state-2/react-state-2_colored-number/src/components/Counter.js
+++ react-state-2/react-state-2_colored-number/src/components/Counter.js
@@ -1,12 +1,23 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [counter, setCounter] = useState(0);
+  function handleClick(event) {
+    if (event.target.textContent == "-") {
+      setCounter(counter - 10);
+    } else {
+      setCounter(counter + 10);
+    }
+  }
+
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={counter} />
       <div className="counter__buttons-container">
         <button
+          onClick={handleClick}
           type="button"
           className="counter__button"
           aria-label="increment count"
@@ -14,6 +25,7 @@ export default function Counter() {
           +
         </button>
         <button
+          onClick={handleClick}
           type="button"
           className="counter__button"
           aria-label="decrement count"
