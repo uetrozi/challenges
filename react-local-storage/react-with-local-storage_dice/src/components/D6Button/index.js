a0423e3cc3c38c0bda0d3862db42f00e659b2467
uetrozi <charlotte_keuer@posteo.de>
2023-12-05 12:45:57 +0100

    finish dice

--- react-local-storage/react-with-local-storage_dice/src/components/D6Button/index.js
+++ react-local-storage/react-with-local-storage_dice/src/components/D6Button/index.js
@@ -1,20 +1,12 @@
-import { useState } from "react";
-import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
+export function D6Button({value, onRoll}) {
 
-  const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
-  };
-
-  const currentRollValue = rolls[0]?.value;
 
   return (
-    <button className="d6-button" type="button" onClick={handleRoll}>
-      <D6 value={currentRollValue} />
+    <button className="d6-button" type="button" onClick={onRoll}>
+      <D6 value={value} />
     </button>
   );
 }
