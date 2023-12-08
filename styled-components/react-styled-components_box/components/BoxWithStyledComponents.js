40b91af25a88baf6cc29174dbffceccba6932ff8
uetrozi <charlotte_keuer@posteo.de>
2023-12-08 15:53:41 +0100

    finish box

---
+++ styled-components/react-styled-components_box/components/BoxWithStyledComponents.js
@@ -0,0 +1,12 @@
+import styled, { css } from "styled-components";
+
+export default styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 2rem;
+  background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
+
+  &:hover {
+    background-color: red;
+  }
+`;
--- styled-components/react-styled-components_box/pages/index.js
+++ styled-components/react-styled-components_box/pages/index.js
@@ -1,10 +1,19 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 export default function HomePage() {
   return (
-    <div>
+    <StyledDiv>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack />
+    </StyledDiv>
   );
 }
