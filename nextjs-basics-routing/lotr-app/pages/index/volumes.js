caf75aa1137b6844a346f5cec06b7263f22c05fe
uetrozi <charlotte_keuer@posteo.de>
2023-12-10 21:46:45 +0100

    finish volumes page

--- nextjs-basics-routing/lotr-app/pages/index/volumes.js
+++ nextjs-basics-routing/lotr-app/pages/index/volumes.js
@@ -1,29 +1,83 @@
 import { introduction, volumes } from "../data";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background-color: var(--color-smoke);
+  padding: 30px;
+`;
+
+const StyledHeading = styled.h1`
+  font: var(--font-headline-1);
+`;
+
+const StyledBody = styled.p`
+  font: var(--font-body);
+`;
+
+const StyledHeading2 = styled.h2`
+  font: var(--font-headline-2);
+`;
+
+const StyledList = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-content: space-evenly;
+  gap: 10%;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`;
+
+const StyledListItem = styled.li``;
+
+const StyledImage = styled.img`
+  box-shadow: var(--box-shadow-book);
+  width: 100%;
+
+  &:hover {
+    box-shadow: var(--box-shadow-book--hover);
+  }
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  font: var(--font-caption);
+  color: var(--color-earth);
+`;
 
 export default function Overview() {
   const router = useRouter();
 
-  function handleRandomVolume() {
-    const randomVolume = volumes[Math.floor(Math.random() * volumes.length)];
-    router.push("/index/volumes/" + randomVolume.slug);
-  }
+  // function handleRandomVolume() {
+  //   const randomVolume = volumes[Math.floor(Math.random() * volumes.length)];
+  //   router.push("/index/volumes/" + randomVolume.slug);
+  // }
 
   return (
-    <div>
-      <h1>Lord of the Rings</h1>
-      <p>{introduction}</p>
-      <h2>All Volumes</h2>
-      <ul>
+    <StyledSection>
+      <StyledHeading>The Lord of the Rings</StyledHeading>
+      <StyledBody>{introduction}</StyledBody>
+      <StyledHeading2>All Volumes</StyledHeading2>
+      <StyledList>
         {volumes.map((volume) => (
           <li key={volume.slug}>
-            <Link href={"/index/volumes/" + volume.slug}>{volume.title}</Link>
+            <StyledLink href={"/index/volumes/" + volume.slug}>
+              <StyledImage
+                src={volume.cover}
+                alt={"Book Cover of " + volume.title}
+              />
+              <br></br>
+
+              {volume.title}
+            </StyledLink>
           </li>
         ))}
-      </ul>
+      </StyledList>
 
-      <button onClick={handleRandomVolume}>Go to random Volume</button>
-    </div>
+      {/* <button onClick={handleRandomVolume}>Go to random Volume</button> */}
+    </StyledSection>
   );
 }
