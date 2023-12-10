a917f460f9c05df3ce21fd180219993bcb14613e
uetrozi <charlotte_keuer@posteo.de>
2023-12-10 17:21:16 +0100

    finish everything except history entry

--- react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
+++ react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import GameForm from "./index";
+import Button from "../Button";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -8,10 +9,63 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  render(<GameForm />);
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+  const input1 = screen.getByLabelText("Name of game");
+  expect(input1).toBeInTheDocument();
 
-test("submits the correct form data when every field is filled out", async () => {});
+  const input2 = screen.getByLabelText("Player names, separated by comma");
+  expect(input2).toBeInTheDocument();
 
-test("does not submit form if one input field is left empty", async () => {});
+  const button = screen.getByRole("button", { name: "Create game" });
+  expect(button).toBeInTheDocument();
+});
+
+test("renders a form with the accessible name 'Create a new game'", () => {
+  render(<GameForm />);
+
+  const form = screen.getByRole("form", { name: "Create a new game" });
+  expect(form).toBeInTheDocument();
+});
+
+test("submits the correct form data when every field is filled out", async () => {
+  const onHandleCreateGame = jest.fn();
+  const user = userEvent.setup();
+  render(<GameForm onCreateGame={onHandleCreateGame} />);
+
+  const input1 = screen.getByLabelText("Name of game");
+
+  const input2 = screen.getByLabelText("Player names, separated by comma");
+
+  const button = screen.getByRole("button", { name: "Create game" });
+  const form = screen.getByRole("form", { name: "Create a new game" });
+
+  await user.type(input1, "abc");
+  await user.type(input2, "def, ghi");
+  await user.click(button);
+
+  expect(onHandleCreateGame).toHaveBeenCalledWith({
+    nameOfGame: "abc",
+    playerNames: ["def", "ghi"],
+  });
+});
+
+test("does not submit form if one input field is left empty", async () => {
+  const onHandleCreateGame = jest.fn();
+  const user = userEvent.setup();
+  render(<GameForm onCreateGame={onHandleCreateGame} />);
+
+  const input1 = screen.getByLabelText("Name of game");
+
+  const input2 = screen.getByLabelText("Player names, separated by comma");
+
+  const button = screen.getByRole("button", { name: "Create game" });
+  const form = screen.getByRole("form", { name: "Create a new game" });
+
+  await user.type(input1, "abc");
+
+  await user.click(button);
+
+  expect(onHandleCreateGame).not.toHaveBeenCalled();
+});
--- react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -2,6 +2,35 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player name="Susaha" />);
+  const buttons = screen.getAllByRole("button");
+  const player = screen.getByText(/susaha/i);
+  expect(player).toBeInTheDocument();
+  expect(buttons).toHaveLength(2);
+});
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+test("calls callbacks when increasing or decreasing score", async () => {
+  const handleDecrease = jest.fn();
+  const handleIncrease = jest.fn();
+  const user = userEvent.setup();
+
+  render(
+    <Player
+      onDecreasePlayerScore={handleDecrease}
+      onIncreasePlayerScore={handleIncrease}
+    />
+  );
+  const decreaseButton = screen.getByRole("button", {
+    name: /Decrease Score/i,
+  });
+  const increaseButton = screen.getByRole("button", {
+    name: /Increase Score/i,
+  });
+
+  await user.click(increaseButton);
+  await user.click(decreaseButton);
+
+  expect(handleDecrease).toHaveBeenCalledTimes(1);
+  expect(handleIncrease).toHaveBeenCalledTimes(1);
+});
