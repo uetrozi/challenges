64f09c66d7733b605404e37da77c3b3bd552a845
uetrozi <charlotte_keuer@posteo.de>
2023-12-20 16:17:17 +0100

    finish ISS Tracker

--- react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,44 +1,38 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
-export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+const fetcher = async (url) => {
+  const res = await fetch(url);
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
   }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  return res.json();
+};
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+export default function ISSTracker() {
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
+  });
+
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
       />
     </main>
   );
