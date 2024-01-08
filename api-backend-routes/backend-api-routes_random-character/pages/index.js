283cc31342965910816fc8ab9179ac1610fc7a1d
uetrozi <charlotte_keuer@posteo.de>
2024-01-08 11:42:04 +0100

    finish random character

--- api-backend-routes/backend-api-routes_random-character/pages/index.js
+++ api-backend-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,28 @@
-export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+export default function RandomCharacter() {
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (!data && !error) return <div>Loading...</div>;
+
+  if (error) return <div>Error fetching data</div>;
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Random Character</h1>
+
+      <p>
+        Name: {data.firstName} {data.lastName}
+      </p>
+      <p>Twitter: {data.twitterName}</p>
+      <p>Geohash: {data.geohash}</p>
+    </div>
+  );
 }
